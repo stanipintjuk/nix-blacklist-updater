ce553b0901bdae7515dfc04565f56f125d3ddc85
Stanislav Pintjuk <stani@stani.se>
2019-02-04 16:42:57 +0000

    Added filter for ipv6

--- update_blacklist.nix
+++ update_blacklist.nix
@@ -26,7 +26,13 @@ ${ipset}/bin/ipset flush "$IP_SET"
 echo "Updating ip:s to $IP_SET ip-set"
 # Create an ip set and add each ip to it one by one
 for IP in $(cat "$BLFILE"); do
-  ${ipset}/bin/ipset add "$IP_SET" $IP
+  # Jump over ipv6 adresses
+  if [[ $IP =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]; then
+    echo "Ignoring IPv6 adress $IP"
+  else 
+    echo "Adding adress $IP"
+    ${ipset}/bin/ipset add "$IP_SET" $IP
+  fi
 done
 
 ''
