56ce6b60c7fd9133838db417d341bb47f69baae6
Michael Lohmann <git@lohmann.sh>
2025-03-06 22:40:43 +0100

    service: refactor config

--- service.nix
+++ service.nix
@@ -1,11 +1,12 @@
 { pkgs, config, ... }:
 with builtins;
-{
+let cfg = config.services.blacklist-updater;
+in {
   systemd.services."blacklist" = {
-    enable = config.services.blacklist-updater.enable;
+    enable = cfg.enable;
     preStart = toString (pkgs.writeScript "init_blacklist.sh" (import ./init_blacklist.nix { inherit pkgs config; }));
     script = toString (pkgs.writeScript "blacklist_update.sh" (import ./update_blacklist.nix { inherit pkgs config; }));
     postStop = toString (pkgs.writeScript "clear_blacklist.sh" (import ./clear_blacklist.nix { inherit pkgs config; }));
-    startAt = config.services.blacklist-updater.updateAt;
+    startAt = cfg.updateAt;
   };
 }
