9f0724c9e3ec426e5883e0d36e9c7117b8f78567
Michael Lohmann <git@lohmann.sh>
2025-03-14 05:14:24 +0100

    update_blacklist: ignore lines not matching patterns

--- update_blacklist.nix
+++ update_blacklist.nix
@@ -31,13 +31,22 @@ ${import ./clear_blacklist.nix { inherit pkgs config; }}
 
 echo "Updating ip:s to ${ipSetName} ip-set"
 # Create an ip set and add each ip to it one by one
-for IP in $(cat "$BLFILE"); do
-  # Jump over ipv6 adresses
-  if [[ $IP =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]; then
-    echo -exist add "${ipV6SetName}" "$IP"
-  else
-    echo -exist add "${ipSetName}" "$IP"
-  fi
-done > "$BLFILE_PROCESSED"
+
+# IPv4 and IPv6 regex patterns with CIDR notation support - WARNING: might not be correct for all IPs (e.g. ignore valid ones or accept wrong ones), but seems to work fine
+ipv4_regex="^([0-9]{1,3}\.){3}[0-9]{1,3}(\/[0-9]{1,2})?$"
+ipv6_regex="^([0-9a-fA-F:]+::?[0-9a-fA-F]*)+(\/[0-9]{1,3})?$"
+
+# Use a temporary buffer to improve performance
+{
+    while IFS= read -r IP; do
+        if [[ $IP =~ $ipv4_regex ]]; then
+            echo -exist add "${ipSetName}" "$IP"
+        elif [[ $IP =~ $ipv6_regex ]]; then
+            echo -exist add "${ipV6SetName}" "$IP"
+        else
+            echo "Warning: Invalid line skipped -> $IP" >&2
+        fi
+    done < "$BLFILE"
+} > "$BLFILE_PROCESSED"
 ${ipset}/bin/ipset restore < "$BLFILE_PROCESSED"
 ''
