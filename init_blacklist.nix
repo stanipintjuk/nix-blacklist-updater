fe05bdfeedfb6a4ebc2d35bf2135278d1abfe65c
Michael Lohmann <git@lohmann.sh>
2025-03-14 04:16:56 +0100

    init_blacklist: refactor
    This allows to do the check for IPv6 as well

--- init_blacklist.nix
+++ init_blacklist.nix
@@ -6,23 +6,20 @@ echo "Running blacklist initializer"
 
 # Stop if the set already exists
 echo "Checking if ip-set ${ipSetName} already exists"
-${ipset}/bin/ipset -L ${ipSetName} >/dev/null 2>&1
-if [ $? -eq 0 ]; then
-  echo "${ipSetName} set already exists. Exiting."
-  exit 0
+
+if ! ${ipset}/bin/ipset -L ${ipSetName} >/dev/null 2>&1; then
+    echo "${ipSetName} doesn't exist. Creating."
+
+    ${ipset}/bin/ipset create "${ipSetName}" hash:ip hashsize 262144
+
+    # Blacklist all addresses from this ip set
+    ${iptables}/bin/iptables -I INPUT -m set --match-set ${ipSetName} src -j DROP
+    ${iptables}/bin/iptables -I INPUT -m set --match-set ${ipSetName} src -j LOG --log-prefix "FW_DROPPED: "
+
+    ${iptables}/bin/iptables -I FORWARD -m set --match-set ${ipSetName} src -j DROP
+    ${iptables}/bin/iptables -I FORWARD -m set --match-set ${ipSetName} src -j LOG --log-prefix "FW_DROPPED: "
+
+    ${iptables}/bin/iptables -t raw -I PREROUTING -m set --match-set ${ipSetName} src -j DROP
+    ${iptables}/bin/iptables -t raw -I PREROUTING -m set --match-set ${ipSetName} src -j LOG --log-prefix "FW_DROPPED: "
 fi
-
-echo "${ipSetName} doesn't exist. Creating."
-
-${ipset}/bin/ipset create "${ipSetName}" hash:ip hashsize 262144
-
-# Blacklist all addresses from this ip set
-${iptables}/bin/iptables -I INPUT -m set --match-set ${ipSetName} src -j DROP
-${iptables}/bin/iptables -I INPUT -m set --match-set ${ipSetName} src -j LOG --log-prefix "FW_DROPPED: "
-
-${iptables}/bin/iptables -I FORWARD -m set --match-set ${ipSetName} src -j DROP
-${iptables}/bin/iptables -I FORWARD -m set --match-set ${ipSetName} src -j LOG --log-prefix "FW_DROPPED: "
-
-${iptables}/bin/iptables -t raw -I PREROUTING -m set --match-set ${ipSetName} src -j DROP
-${iptables}/bin/iptables -t raw -I PREROUTING -m set --match-set ${ipSetName} src -j LOG --log-prefix "FW_DROPPED: "
 ''
