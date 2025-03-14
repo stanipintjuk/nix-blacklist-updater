b1c49bbf8bef6f99760387cf7be18b53d318a442
Michael Lohmann <git@lohmann.sh>
2025-03-14 04:52:08 +0100

    update_blacklist: only open BLFILE_PROCESSED once
    This should improve performance a lot

--- update_blacklist.nix
+++ update_blacklist.nix
@@ -16,7 +16,6 @@ BLFILE_PROCESSED="/tmp/ipblacklist_processed.txt"
 
 # Empty the output file if it exists
 > "$BLFILE"
-> "$BLFILE_PROCESSED"
 
 # Download the blacklist and add it to a file
 for url in "''${urls[@]}"; do
@@ -35,10 +34,10 @@ echo "Updating ip:s to ${ipSetName} ip-set"
 for IP in $(cat "$BLFILE"); do
   # Jump over ipv6 adresses
   if [[ $IP =~ ^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$ ]]; then
-    echo -exist add "${ipV6SetName}" "$IP" >> "$BLFILE_PROCESSED"
+    echo -exist add "${ipV6SetName}" "$IP"
   else
-    echo -exist add "${ipSetName}" "$IP" >> "$BLFILE_PROCESSED"
+    echo -exist add "${ipSetName}" "$IP"
   fi
-done
+done > "$BLFILE_PROCESSED"
 ${ipset}/bin/ipset restore < "$BLFILE_PROCESSED"
 ''
